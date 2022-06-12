e47f726bfa21868c6495ed2fbd8e0423003c4893
juan <juan.fogliato@gmail.com>
2022-06-12 19:04:18 -0300

    create ciudades ok

--- app/controllers/ciudad.controller.js
+++ app/controllers/ciudad.controller.js
@@ -5,7 +5,7 @@ const Op = db.Sequelize.Op;
 // Create and Save a new ciudad
 exports.create = (req, res) => {
   // Validate request
-  if (!req.body.title) {
+  if (!req.body.nombre) {
     res.status(400).send({
       message: "Content can not be empty!"
     });
