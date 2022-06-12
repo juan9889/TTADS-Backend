c6aaf8ca1cd58a708205180ac46313bb263809c0
juan <juan.fogliato@gmail.com>
2022-06-12 12:38:17 -0300

    ni idea

--- ttads-backend/routes/ciudades.js
+++ ttads-backend/routes/ciudades.js
@@ -1,13 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('comunidades', 'ttads', 'ttads', {
-  dialect: 'mysql',
-  dialectOptions: {
-    // Your mysql2 options here
-    host: 'localhost'
-  }
-})
+
+const sequelize = require('sequelize');
 //esto anda, tendriamos que ver una forma de traer sequelize sin meter este codigo en todos lados
 
 let probar = async () =>{
@@ -25,7 +19,7 @@ router.get('/', function(req, res, next) {
     res.send('ruta ciudades');
   });
 
-  router.get('/get_ciudades', function(req, res, next) {
+router.get('/get_ciudades', function(req, res, next) {
     probar();
     res.send('este deberia devolver todas las ciudades');
   });
