8ee43ee764382545fd16228a1bb1e42a2b94a502
Cristian Gofiar <cristiangofiar@gmail.com>
2022-09-27 23:56:24 -0300

    Controlador de provincias actualizado.

--- app/controllers/province.controller.js
+++ app/controllers/province.controller.js
@@ -15,7 +15,7 @@ exports.create = async (req, res) => {
     await Province.create({ name: req.body.name });
     res.status(201).send({ message: 'Created', data: req.body, status: 201 });
   } catch (error) {
-    res.status(500).send({ message: error.name + error.message });
+    res.status(500).send({ message: error.name + ': ' + error.message });
   }
 };
 
@@ -31,7 +31,7 @@ exports.findAll = async (req, res) => {
       res.status(404).send({ message: 'Cannot find' });
     }
   } catch (error) {
-    res.status(500).send({ message: error.name + error.message });
+    res.status(500).send({ message: error.name + ': ' + error.message });
   }
 };
 
@@ -50,94 +50,62 @@ exports.findOne = async (req, res) => {
       res.status(404).send({ message: `Cannot find with id=${req.params.id}.` });
     }
   } catch (error) {
-    res.status(500).send({ message: error.name + error.message });
+    res.status(500).send({ message: error.name + ': ' + error.message });
   }
 };
 
 // Update by the id in the request
-exports.update = (req, res) => {
-  if (!req.params.id) {
-    res.status(400).send({
-      message: "id can not be empty!"
-    });
-    return
+exports.update = async (req, res) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).send({ message: "id can not be empty!" });
+      return
+    };
+    const id = req.params.id;
+    var province = await Province.update(req.body, { where: { id: id } });
+    if (province == 1) {
+      res.status(200).send(req.body);
+    } else {
+      res.status(404).send({ message: `Cannot update provincia with id=${id}. Maybe provincia was not found or req.body is empty!` });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.name + ': ' + error.message });
   }
-  const id = req.params.id;
-
-  Province.update(req.body, {
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.status(200).send({
-          message: "provincia was updated successfully."
-        });
-      } else {
-        res.status(404).send({
-          message: `Cannot update provincia with id=${id}. Maybe provincia was not found or req.body is empty!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.name + ': ' + err.message || "Error updating provincia with id=" + id
-      });
-    });
 };
 
 // Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {
-  if (!req.params.id) {
-    res.status(400).send({
-      message: "id can not be empty!"
-    });
-    return
+exports.delete = async (req, res) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).send({ message: "id can not be empty!" });
+      return
+    };
+    const id = req.params.id;
+    var province = await Province.destroy({ where: { id: id } });
+    if (province == 1) {
+      res.status(200).send({ message: "The province was deleted successfully!" });
+    } else {
+      res.status(404).send({ message: `Cannot delete provincia with id=${id}. Maybe provincia was not found!` });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.name + ': ' + error.message });
   }
-  const id = req.params.id;
-
-  Province.destroy({
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.status(200).send({
-          message: "The province was deleted successfully!"
-        });
-      } else {
-        res.status(404).send({
-          message: `Cannot delete provincia with id=${id}. Maybe provincia was not found!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.name + ': ' + err.message || "Could not delete provincia with id=" + id
-      });
-    });
 };
 
-exports.findCities = (req, res) => {
-  if (!req.params.id) {
-    res.status(400).send({
-      message: "id can not be empty!"
-    });
-    return
+exports.findCities = async (req, res) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).send({ message: "id can not be empty!" });
+      return
+    };
+    const id = parseInt(req.params.id);
+    var cities = await Province.scope({ method: ['cities', id] }).findAll();
+    if (cities && cities.length > 0) {
+      res.status(200).send(cities);
+    } else {
+      res.status(404).send({ message: `Cannot find cities in the province with id=${id}.` })
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.name + ': ' + error.message });
   }
-
-  const id = parseInt(req.params.id);
-  Province.scope({ method: ['cities', id] }).findAll()
-    .then(data => {
-      if (data) {
-        res.status(200).send(data);
-      }
-      else {
-        res.status(404).send({ message: 'Cannot find' })
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.name + ': ' + err.message || "Some error occurred while retrieving "
-      });
-    });
 };
