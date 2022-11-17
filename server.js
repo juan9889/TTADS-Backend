6ebca0bdee42d179dc9c34f798f8147488304b19
Juan Agustin Fogliato <48728949+juan9889@users.noreply.github.com>
2022-11-17 14:16:46 -0300

    intento 2 oauth

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require("axios")
 const cors = require('cors')
 require('dotenv').config()
 const testdata = require('./app/config/test_data.js')
@@ -10,6 +11,10 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const CLIENT_ID = "30922afb2cdc1a52ad8f";
+const CLIENT_SECRET = "6862513c2ae3d4330de06fb4e7088455eaf9ce71";
+const GITHUB_URL = "https://github.com/login/oauth/access_token";
+
 // Routes
 require('./app/routes')(app)
 
@@ -19,6 +24,20 @@ sequelize.sync({ force: false })
 app.get('/', (req, res) => {
   res.json({ message: 'Bienvenido a la api de comunidades' })
 })
+
+app.get("/oauth/redirect", (req, res) => {
+  axios({
+    method: "POST",
+    url: `${GITHUB_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`,
+    headers: {
+      Accept: "application/json",
+    },
+  }).then((response) => {
+    res.redirect(
+      `http://localhost:3000?access_token=${response.data.access_token}`
+    );
+  });
+});
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080
