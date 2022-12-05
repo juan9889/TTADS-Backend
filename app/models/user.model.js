992b4adb2ba3a85ab33ed53b169175c8b3dbd7ca
Cristian Gofiar <cristiangofiar@gmail.com>
2022-12-05 15:07:07 -0300

    asd

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -10,10 +10,93 @@ module.exports = (sequelize) => {
     username: {
       type: Sequelize.DataTypes.STRING
     },
-    user_password: {
+    used_oauth: {
+      type: Sequelize.DataTypes.BOOLEAN
+    },
+    password: {
+      type: Sequelize.DataTypes.STRING
+    },
+    name: {
+      type: Sequelize.DataTypes.STRING
+    },
+    mail: {
       type: Sequelize.DataTypes.STRING
     }
-
+  }, {
+    scopes: {
+      find () {
+        return {
+          attributes: {
+            exclude: ['password', 'cityId']
+          },
+          include: [
+            {
+              model: sequelize.models.city,
+              // required: true, descomentar cuando resolvamos le problema de que todos deben tener ciudad
+              attributes: ['id', 'name'],
+              include: {
+                model: sequelize.models.province,
+                // required: true,
+                attributes: ['id', 'name']
+              }
+            }
+          ]
+        }
+      },
+      events () {
+        return {
+          // falta completar
+          include: [
+            {
+              model: sequelize.models.event,
+              required: true,
+              attributes: ['id', 'title', 'date', 'place', 'description', 'time', 'state'],
+              include: [
+                {
+                  model: sequelize.models.community,
+                  required: true,
+                  attributes: ['id', 'name']
+                },
+                {
+                  model: sequelize.models.event_category,
+                  required: true,
+                  attributes: ['id', 'name', 'icon', 'iconColor']
+                },
+                {
+                  model: sequelize.models.city,
+                  required: true,
+                  attributes: ['id', 'name'],
+                  include: {
+                    model: sequelize.models.province,
+                    required: true,
+                    attributes: ['id', 'name']
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      },
+      communities () {
+        return {
+          // falta completar
+          include: [{
+            model: sequelize.models.community,
+            required: true,
+            attributes: ['id', 'name'],
+            include: {
+              model: sequelize.models.comm_category,
+              required: true,
+              attributes: ['id', 'name', 'icon', 'iconColor']
+            }
+          },
+          {
+            model: sequelize.models.user_community,
+            required: true
+          }]
+        }
+      }
+    }
   })
   return user
 }
